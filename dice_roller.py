275b0cb92fe8fa9d820abd29a63c11a3c6b37bc8
thomurie <70548271+thomurie@users.noreply.github.com>
2020-09-18 22:45:25 -0600

    Added enhance UX features

--- dice_roller.py
+++ dice_roller.py
@@ -6,8 +6,18 @@ def main():
   for i in range(0,dice_rolls):
     roll = random.randint(1,6)
     dice_sum += roll
-    print(f'You rolled a {roll}')
-  print(f'You have rolled a total of {dice_sum}')
+    if roll == 1:
+      print(f'You rolled a {roll}, critical failure')
+    elif roll == 6:
+      print(f'You rolled a {roll}! Critical Success!')
+    else:
+      print(f'You rolled a {roll}')
+  if dice_sum == 2:
+    print(f'You have rolled 2, snake eyes!')
+  elif dice_sum == 7:
+    print(f'Lucky Seven')
+  else:
+    print(f'You have rolled a total of {dice_sum}')
 
 if __name__== "__main__":
   main()
