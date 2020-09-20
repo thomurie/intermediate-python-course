6e24e90a5a10bc6d48c60f3cd77b0ad640752731
thomurie <70548271+thomurie@users.noreply.github.com>
2020-09-20 10:17:32 -0600

    Dice_roller is now Lucky Seven

--- dice_roller.py
+++ dice_roller.py
@@ -1,28 +1,48 @@
+import time
+import sys
 
-def main():
+def delay_print(s):
+    #function slows down the text like a gameboy
+    for c in s:
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(0.05)
+# title
+delay_print("Welcome to Lucky Seven \n")
+# Number of Players
+number_of_players = int(input('How many players are playing?  '))
+players = list(range(1, number_of_players+1))
+#rules
+delay_print("The rules are simple: \n The first player throws the dice. \n If they roll a 7, the player to the right of the roller takes a shot. \n If the roll is not 7, then the roller passes the dice to the left. \n If a player rolls snake eyes everyone takes a shot \n")
+#invite
+invite = str(input("Are you ready to play? Yes/No "))
+
+#Dice Roll
+def turn():
   import random
-
-  dice_rolls = int(input('How many dice would you like to roll?'))
-  die_size = int(input('How many sides does each die have?'))
+  die_size = 6
   dice_sum = 0
 
-  for i in range(0,dice_rolls):
+  for i in range(0,2):
     roll = random.randint(1,die_size)
     dice_sum += roll
-    if roll == 1:
-      print(f'You rolled a {roll}, critical failure')
-    elif roll == die_size:
-      print(f'You rolled a {roll}! Critical Success!')
-    else:
-      print(f'You rolled a {roll}')
+    delay_print(f'You rolled a {roll}\n')
   if dice_sum == 2:
-    print(f'You have rolled 2, snake eyes!')
+    delay_print(f'SNAKE EYES !!!EVERYBODY TAKES A SHOT!!!\n')
   elif dice_sum == 7:
-    print(f'Lucky Seven')
+    delay_print(f'Lucky Seven\n')
   else:
-    print(f'You have rolled a total of {dice_sum}')
+    delay_print(f'You have rolled a total of {dice_sum}, Next Player\n')
 
-if __name__== "__main__":
-  main()
-
-
+#Game Play
+active= True
+while active:
+  while players:
+    for player in players:
+      start = str(input(f'Player {player} are you ready to roll? (Yes/No)\n'))
+      if start.lower() == "yes":
+        turn()
+  else:
+    delay_print("Thanks for Playing!, Please Drive Home Safely!")
+    active= False
+  continue
